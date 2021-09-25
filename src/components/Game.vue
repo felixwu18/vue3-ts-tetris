3b84b1dd62a9b7d40ec67cb9782bd7489a0b1e39
felixwu18 <renai107@163.com>
2021-09-25 16:53:02 +0800

    将映射的type通过地图map分别控制实现方块的变色-检验实现

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,7 +3,7 @@
   <div>
     <div class="row" v-for="row in gameRow" :key="row">
       <div v-for="col in gameCol" :key="col">
-        <Box :type="0"></Box>
+        <Box :type="map[row - 1][col - 1]"></Box>
       </div>
     </div>
   </div>
@@ -14,7 +14,23 @@
 import Box from './Box.vue'
 import { gameRow, gameCol } from '../game' // 业务逻辑与视图分开
 
+// map 地图
+const map = []
 
+// initMap 初始化
+for(let i = 0; i < gameRow; i++) {
+  const arr = []
+    for(let j = 0; j < gameRow; j++) {
+        arr.push(0)
+    }
+  map.push(arr)
+}
+
+// 实验map逻辑使得方块变色
+map[0][0] = 1
+map[0][1] = 1
+map[0][2] = 1
+map[0][3] = 1
 
 </script>
 
