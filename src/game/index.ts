35838ca4841bbfd7edc1994c3d4fa1fd0afa730c
felixwu18 <renai107@163.com>
2021-09-25 17:37:43 +0800

    给游戏对象box添加位置属性

--- src/game/index.ts
+++ src/game/index.ts
@@ -14,10 +14,16 @@ initMap()
 
 const box = new Box()
 
+box.x = 0
+box.y = 2
+
+
 //  box => map => 1 让box赋值给map 让对于type变为1 实现变色
 for(let i = 0; i < box.shape.length; i++) {
     const arr: Array<number> = []
       for(let j = 0; j < box.shape[0].length; j++) {
-        map[i][j] = 1
+        const row = i + box.y
+        const col = j + box.x
+        map[row][col] = 1
       }
-  }
+}
