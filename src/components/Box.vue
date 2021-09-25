7e9ddadacf54bddb7c931e82148e56ca36926589
felixwu18 <renai107@163.com>
2021-09-25 16:06:48 +0800

    将1代替的小方块抽离成组件并展现出方块排列

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+  <div class="box">
+    1
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
+<style>
+.box {
+    width: 20px;
+    height: 20px;
+    background: gray;
+    margin: 2px;
+}
+</style>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,13 +3,17 @@
   <div>
     <div class="row" v-for="row in 10" :key="row">
       <div v-for="col in 10" :key="col">
-        1
+        <Box></Box>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+// @ts-ignore 解决 is not a module.Vetur
+import Box from './Box.vue'
+
+
 
 </script>
 
