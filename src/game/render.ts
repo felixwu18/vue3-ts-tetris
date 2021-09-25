c8ef1dcd5a7d50e7c227d533deb6224481322afb
felixwu18 <renai107@163.com>
2021-09-25 18:11:10 +0800

    每次调用render时先重置map

--- src/game/render.ts
+++ src/game/render.ts
@@ -1,5 +1,13 @@
 
 export function render(box, map)  {
+
+  // 重置map
+  for(let i = 0; i < map.length; i++) {
+      for(let j = 0; j < map.length; j++) {
+        map[i][j] = 0
+      }
+  }
+
   //  box => map => 1 让box赋值给map 让对于type变为1 实现变色
   for(let i = 0; i < box.shape.length; i++) {
       const arr: Array<number> = []
